d9e59952d98096c8716d6ada0bfac72131ce0fb3
Namita Shenai <namita.shenai@gmail.com>
2019-03-18 18:21:24 -0700

    Adding the node-inquirer with package.json

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# liri-node-app
-LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+# LIRI Node Application 
+    LIRI is Language Interpretation and Recognition Interface
+
+LIRI is a command line node app that takes command line parameters. Based on the selected parameters calls the APIs and returns da†a in JSON format.
+
+
+### Prerequisites
+To run this application you will need Node.JS and NPM installed on your system.
+
+Inside of the folder in which you've cloned the files to, run the following command:
+
+    npm install
+
+Inside of the folder in which you've cloned the files to, run the following command:
+
+    npm init -y 
+
+    npm install inquirer
+
+### Running the application 
+
+    node liri.js
+
+### Built With : 
+1. Node.JS 
+    1.1 node-inquirer 
+2. Spotify API 
+3. OMDB API 
--- liri.js
+++ liri.js
@@ -1,8 +1,41 @@
 //code to read and set any environment variables with the dotenv package
-require("dotenv").config();
+// require("dotenv").config();
 
 //Add the code required to import the keys.js file and store it in a variable.
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
+
+// Core node package for reading and writing files
+var fs = require("fs");
+
+//A simple to use API library for the Spotify REST API.
+var Spotify = require('node-spotify-api');
 
 //able to access your keys information
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+
+// Load the NPM Package inquirer
+var inquirer = require("inquirer");
+
+// Create a "Prompt" with a series of questions.
+inquirer
+  .prompt([
+    // Here we create a basic text prompt.
+    {
+      type: "input",
+      message: "Welcome, please type in your name??",
+      name: "username"
+    },
+    // Here we create a list to select prompt. 
+    {
+        type: "list",
+        message: "How can I help you today? \n The available commands are:",
+        choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
+        name: "action"
+      }
+    ]) .then(function(inquirerResponse) {
+    // If the inquirerResponse confirms, we displays the inquirerResponse's  from the answers.
+    console.log("\nWelcome " + inquirerResponse.username);
+    console.log("chosen action: "+inquirerResponse.action);
+
+});
+      
