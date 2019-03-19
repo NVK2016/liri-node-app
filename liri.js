40dd0c9f37e853fa03bcfb9d9c0403c642322e52
Namita Shenai <namita.shenai@gmail.com>
2019-03-18 23:30:55 -0700

    Added the Bands in Twon APi calling

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//////////////-----------VARIABLES-----------------///////////////////////////////////////
 //code to read and set any environment variables with the dotenv package
 require("dotenv").config();
 
@@ -16,10 +17,12 @@ var spotify = new Spotify(keys.spotify);
 //Grabs data from axios to access API 
 var axios = require("axios");
 
-console.log(spotify);
+// console.log(spotify);
 
 // Load the NPM Package inquirer
 var inquirer = require("inquirer");
+
+//////////////--------------END-OF-VARIABLES--------------///////////////////////////////////////
 
 // Create a "Prompt" with a series of questions.
 inquirer
@@ -40,42 +43,53 @@ inquirer
     ]) .then(function(inquirerResponse) {
     // If the inquirerResponse confirms, we displays the inquirerResponse's  from the answers.
     console.log("\nWelcome " + inquirerResponse.username);
-    console.log("Choosen action: "+inquirerResponse.action);
+    // console.log("Choosen action: "+inquirerResponse.action);
     
     //Based on the user input call the function 
-  switch(inquirerResponse.action){
-    case "concert-this":
-      console.log("concert-this");
-      break;
-    case "spotify-this-song":
-      console.log("spotify-this-song");
-      break;
-    case "movie-this":
-      console.log("movie-this");
-      inquirer
-      .prompt([
-      // Here we create a basic text prompt.
-      {
-        type: "input",
-        message: "Please enter movie name ?",
-        name: "movieName"
-      }]) .then(function(result) {
-        // If the inquirerResponse confirms, we displays the inquirerResponse's  from the answers.
-        console.log("\nWelcome " + inquirerResponse.username);
-        console.log("Movie name: "+result.movieName);
+    switch(inquirerResponse.action){
+      case "concert-this":
+        // console.log("concert-this");
+        inquirer
+        .prompt([
+        // Here we create a basic text prompt.
+        {
+          type: "input",
+          message: "Please enter the artist, to know more about the upcoming event(s)",
+          name: "concertName"
+        }]) 
+        .then(function(result) {
+            concertThis(result.concertName); 
+        });
+        break;
+      case "spotify-this-song":
+        console.log("spotify-this-song");
+        break;
+      case "movie-this":
+        console.log("movie-this");
+        inquirer
+        .prompt([
+        // Here we create a basic text prompt.
+        {
+          type: "input",
+          message: "Please enter movie name ?",
+          name: "movieName"
+        }]) .then(function(result) {
+          // console.log("\nWelcome " + inquirerResponse.username);
+          // console.log("Movie name: "+result.movieName);
 
-        //Call Movie function 
-        movieThis(result.movieName); 
+          //Call Movie function once a user enters a movie name 
+          movieThis(result.movieName); 
 
-      });
-      break;
-    case "do-what-it-says":
-      doWhatItSays()
-      break;
-    default: 
-    console.log("do-what-it-says");
-      break; 
-  }
+        });
+        break;
+      case "do-what-it-says":
+        doWhatItSays();
+        break;
+      default: 
+        console.log("do-what-it-says");
+        doWhatItSays();
+        break; 
+    }
 
 });
       
@@ -128,13 +142,13 @@ function movieThis(inputMovie){
       // Then log the body from the site!
       // console.log(response.data);
       // var cssStyle = "color:'DodgerBlue';font-weight:bold; "; 
-      console.log("\n Title: " + response.data.Title );
-      console.log("\n Release Year: " + response.data.Year);
+      console.log("\n * Title of the movie: " + response.data.Title );
+      console.log("\n * Year the movie came out: " + response.data.Year);
       console.log("\n IMDB Rating: " + response.data.Ratings[0].Value);
       console.log("\n Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
       console.log("\n Country: " + response.data.Country);
       console.log("\n Language(s): " + response.data.Language);
-      console.log("\n Plot: " + response.data.Plot);
+      console.log("\n  Plot of the movie: " + response.data.Plot);
       console.log("\n Actors: " + response.data.Actors);
     })
   //otherwise log error - Follow the docs. 
@@ -143,4 +157,44 @@ function movieThis(inputMovie){
       console.log(error);
     }
   );
+}
+
+//Data related to concert details 
+function concertThis(inputArtist){
+
+  console.log(inputArtist);
+  //If the user doesn't type a artist in, the program will output data for the movie 'Lady gaga'
+  if (inputArtist === undefined || inputArtist === '')  {
+    inputArtist = "Lady Gaga";
+
+  }
+  // Then run a request with axios to the OMDB API with the movie specified
+  var queryUrl = "https://rest.bandsintown.com/artists/" + inputArtist.split(' ').join('%20') + "/events?app_id=codingbootcamp"
+
+  // This line is just to help us debug against the actual URL.
+  console.log(queryUrl);
+
+  axios.get(queryUrl)
+  .then(
+    function(response) {
+      console.log(response.data.length); 
+      for( var i = 0; i < response.data.length; i++){
+        console.log("-----------EVENT Details ---------------\n");
+        console.log("\n Name of the venue: " + response.data[i].venue.name );
+        console.log("\n Venue Location: " + response.data[i].venue.city);
+        //Using Moment to format Date 
+        console.log("\n Date of the Event: " + response.data[i].datetime);
+        console.log("----------------------\n");
+      }
+    })
+  //otherwise log error - Follow the docs. 
+  .catch(function (error) {
+      console.log('Hello - Concert function')
+      console.log(error);
+    }
+  );
+}
+
+//
+function spotifyThis(){
 }
